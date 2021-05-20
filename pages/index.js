7ff0758bceba736c96eb99a1c59158eb956355c8
Juan Enrique Segebre Zaghmout <10774915+Segebre@users.noreply.github.com>
2021-05-21 01:06:39 +0200

    Created site template in App

--- pages/index.js
+++ pages/index.js
@@ -1,70 +1,36 @@
-import Head from 'next/head';
-import SafeLink from '../src/components/safe-link';
 import NoPrefetchLink from '../src/components/no-prefetching-link';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Next.js Pre-rendering</title>
-        <link
-          rel="icon"
-          href={`${process.env.__NEXT_ROUTER_BASEPATH}/favicon.png`}
-        />
-      </Head>
+    <>
+      <h1 className={styles.title}>Pre-rendering using Next.js</h1>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Pre-rendering using Next.js</h1>
+      <p className={styles.description}>
+        Check out the different types of renderings available when using Next.js
+      </p>
 
-        <p className={styles.description}>
-          Check out the different types of renderings available when using
-          Next.js
-        </p>
+      <div className={styles.grid}>
+        <NoPrefetchLink href="/about" className={styles.card}>
+          <h2>About &rarr;</h2>
+          <p>Static Generation without data.</p>
+        </NoPrefetchLink>
 
-        <div className={styles.grid}>
-          <NoPrefetchLink href="/about" className={styles.card}>
-            <h2>About &rarr;</h2>
-            <p>Static Generation without data.</p>
-          </NoPrefetchLink>
+        <NoPrefetchLink href="/no-prerendering" className={styles.card}>
+          <h2>About &rarr;</h2>
+          <p>Without pre-rendering.</p>
+        </NoPrefetchLink>
 
-          <NoPrefetchLink href="/no-prerendering" className={styles.card}>
-            <h2>About &rarr;</h2>
-            <p>Without pre-rendering.</p>
-          </NoPrefetchLink>
+        <NoPrefetchLink href="/posts" className={styles.card}>
+          <h2>Posts &rarr;</h2>
+          <p>Static Generation with data.</p>
+        </NoPrefetchLink>
 
-          <NoPrefetchLink href="/posts" className={styles.card}>
-            <h2>Posts &rarr;</h2>
-            <p>Static Generation with data.</p>
-          </NoPrefetchLink>
-
-          <NoPrefetchLink href="/profile" className={styles.card}>
-            <h2>Profile &rarr;</h2>
-            <p>Server-side Rendering.</p>
-          </NoPrefetchLink>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <span>Developed by</span>
-        <span>Juan Enrique Segebre Zaghmout</span>
-        <div>
-          <SafeLink href="https://github.com/Segebre" noA11yIcon>
-            <img
-              src={`${process.env.__NEXT_ROUTER_BASEPATH}/github.png`}
-              alt="Juan's GitHub page"
-              className={styles.logo}
-            />
-          </SafeLink>
-          <SafeLink href="https://de.linkedin.com/in/segebre" noA11yIcon>
-            <img
-              src={`${process.env.__NEXT_ROUTER_BASEPATH}/linkedin.png`}
-              alt="Juan's LinkedIn page"
-              className={styles.logo}
-            />
-          </SafeLink>
-        </div>
-      </footer>
-    </div>
+        <NoPrefetchLink href="/profile" className={styles.card}>
+          <h2>Profile &rarr;</h2>
+          <p>Server-side Rendering.</p>
+        </NoPrefetchLink>
+      </div>
+    </>
   );
 }
