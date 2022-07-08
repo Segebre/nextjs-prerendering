f3c7174851dc30903b4830603d45caba60cf7ca5
segebre <10774915+Segebre@users.noreply.github.com>
2022-07-08 15:49:46 +0200

    Demo quick fixes

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,6 @@
 // Static Generation with data
 
+import { useEffect } from 'react';
 import styles from '../../styles/Posts.module.css';
 import { SafeLink } from '../../src/components/safe-link';
 import NoPrefetchLink from '../../src/components/no-prefetching-link';
@@ -8,7 +9,7 @@ import NoPrefetchLink from '../../src/components/no-prefetching-link';
 // https://nextjs.org/docs/basic-features/data-fetching#getstaticprops-static-generation
 export async function getStaticProps() {
   const res = await fetch(
-    'https://607b6ab567e6530017573130.mockapi.io/posts?sortBy=id&order=desc'
+    `https://${process.env.NEXT_PUBLIC_MOCK_API_KEY}.mockapi.io/posts?sortBy=id&order=desc`,
   );
   const posts = await res.json();
 
@@ -20,16 +21,23 @@ export async function getStaticProps() {
 
   return {
     props: { posts }, // will be passed to the page component as props
-    revalidate: 10, // time in seconds to re-run getStaticProps (re-generate the page)
+    // revalidate: 10, // time in seconds to re-run getStaticProps (re-generate the page)
   };
 }
 
 function Posts({ posts }) {
+  useEffect(() => {
+    fetch(
+      `https://${process.env.NEXT_PUBLIC_MOCK_API_KEY}.mockapi.io/posts?sortBy=id&order=desc`,
+    );
+  });
+
   return (
     <div className={styles.content}>
       <h1 aria-describedby="disclaimer">Posts</h1>
       <p id="disclaimer">
-        All the data on this page is fetched from{' '}
+        All the data on this page is fetched from
+        {' '}
         <SafeLink href="https://www.mockapi.io/">mockapi.io</SafeLink>
       </p>
       <ul className={styles.list}>
